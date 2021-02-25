8720afa51cd3784522e6e41b6b3da8a3bf6468fe
murselaysan <64005116+murselaysan@users.noreply.github.com>
2021-02-25 00:43:03 +0000

    Update App js, imported footer and header component

--- src/App.js
+++ src/App.js
@@ -1,76 +1,68 @@
-import React , {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import Footer from './Footer';
+import Header from './Header';
 import Magazine from './Magazine';
-import logo from "./logo-2018.svg"
 
 function App() {
+	const APP_KEY = '8d8e00879669413c882aeca7eaab3997';
 
-  const APP_KEY = "8d8e00879669413c882aeca7eaab3997";
-  
-    const[news,setNews] = useState([]);
+	const [ news, setNews ] = useState([]);
 
-    const[search,setSearch] = useState("");
+	const [ search, setSearch ] = useState('');
 
-    const[query,setQuery] = useState("fun")
+	const [ query, setQuery ] = useState('fun');
 
-    useEffect(()=>{
-      getNews();
- },[query])
+	useEffect(
+		() => {
+			getNews();
+		},
+		[query]
+	);
 
-const getNews = async() => {
-const response = await fetch(`https://newsapi.org/v2/everything?q=${query}&from=2021-02-24&sortBy=popularity&pageSize=10&apiKey=${APP_KEY}`);
-const data = await response.json();
+	const getNews = async () => {
+		const response = await fetch(
+			`https://newsapi.org/v2/everything?q=${query}&from=2021-02-24&sortBy=popularity&pageSize=10&apiKey=${APP_KEY}`
+		);
+		const data = await response.json();
 
-setNews(data.articles);
-console.log(data.articles)
+		setNews(data.articles);
+		console.log(data.articles);
+	};
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+		console.log(search);
+	};
 
-} 
-const updateSearch =e=>{
-setSearch(e.target.value)
-console.log(search)
-}
+	const getSearch = (e) => {
+		
+		e.preventDefault();
+    if (search.trim() === '') return false
+		setQuery(search.toLowerCase);
+		setSearch('');
+	};
 
-const getSearch =e=>{
-e.preventDefault();
-setQuery(search);
-setSearch("")
-}
-
-
-
-
-
-
-
-
-
-  return (
-    <div className="App">
-      <div className ="fixed-header"> 
-      <img  className = "logo"src={logo} alt = ""/>
-     
-      
-       </div>
-       <h1 className = "header-text">BT React Code Test - by Mursel  Aysan - 25/02/21</h1>
-
-      <form onSubmit ={getSearch} className ="search-form">
-        <input className ="search-bar" type ="text" value ={search} onChange = {updateSearch}/>
-        <button  className ="search-button" type ="submit">Search</button>
-      </form>
-      {news.map((item,index)=>(
-        <Magazine
-        key={index} 
-        title = {item.title} 
-        author = {item.author}
-        description = {item.description}
-        urlToImage= {item.urlToImage}
-        url = {item.url}
-        />
-      ))}
-      <div className = "fixed-footer"> <h3>© All rigths reserved | Mursel AYSAN</h3>  </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header getSearch={getSearch} updateSearch={updateSearch} search={search}/>
+      <div className="h1">
+        <h1>BT React Code Test - by Mursel AYSAN - 25/02/21</h1>
+      </div>
+      <div className="row">
+      {news && news.map((item, index) => (
+				<Magazine
+					key={index}
+					title={item.title}
+					author={item.author}
+					content={item.content}
+					urlToImage={item.urlToImage}
+					url={item.url}
+				/>
+			))}
+      </div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
