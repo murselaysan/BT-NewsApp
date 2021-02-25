af042ce25cdaa90bbb275b81c41bd13f2ff4aed4
murselaysan <64005116+murselaysan@users.noreply.github.com>
2021-02-25 00:39:59 +0000

    Footer Js added again

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className="fixed-footer">
+           © All rigths reserved | Mursel AYSAN News App
+        </div>
+    )
+}
